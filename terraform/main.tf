4874f21e9fdaa8f1d91230f87c510765b4ad834c
Bartlomiej Wilczynski <me@bwilczynski.com>
2019-04-19 21:16:05 +0200

    Add CloudFront distribution

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,3 +31,52 @@ resource "aws_acm_certificate" "certificate" {
   domain_name       = "${var.domain_name}"
   validation_method = "EMAIL"
 }
+
+resource "aws_cloudfront_distribution" "website_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.website.website_endpoint}"
+    origin_id   = "${var.domain_name}"
+
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "${var.domain_name}"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  aliases = ["${var.domain_name}"]
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${aws_acm_certificate.certificate.arn}"
+    ssl_support_method  = "sni-only"
+  }
+}
